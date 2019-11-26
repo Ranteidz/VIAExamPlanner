c07ec1a54f1c1db0d55be61514dda8cc208d6c72
Ranteidz <55162517+Ranteidz@users.noreply.github.com>
2019-11-26 22:42:59 +0100

    Added a method tester button in gui

--- src/main/java/planner/PrimaryController.java
+++ src/main/java/planner/PrimaryController.java
@@ -1,7 +1,12 @@
 package planner;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import model.Beans.Student;
+import model.Dao.StudentDao;
 import model.DataModel;
 
 public class PrimaryController {
@@ -11,5 +16,15 @@ private DataModel model;
     public void setModel(DataModel model)
     {
         this.model = model;
+    }
+
+    public void MethodTesting(ActionEvent actionEvent)
+    {
+        StudentDao dao = new StudentDao();
+        ArrayList<Student> students = DataModel.getStudentAll();
+
+        for(Student member : students){
+            System.out.println(member);
+        }
     }
 }
