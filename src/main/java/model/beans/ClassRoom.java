1bb52a9e2aea753fc04922ebe7bb69153322e03f
glad1o <2000Rulet?isabadoass>
2019-11-27 23:40:04 +0100

    Classroom fixed

--- src/main/java/model/beans/ClassRoom.java
+++ src/main/java/model/beans/ClassRoom.java
@@ -1,56 +1,71 @@
 package model.beans;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class ClassRoom {
-    private String name;
-    private int capacity;
-    private boolean hasHDMI;
-    private boolean hasVGA;
-    private boolean isAvailable;
+    private StringProperty name = new SimpleStringProperty();
+    private IntegerProperty capacity = new SimpleIntegerProperty();
+    private BooleanProperty hdmi = new SimpleBooleanProperty();
+    private BooleanProperty vga = new SimpleBooleanProperty();
 
-    public ClassRoom()
-    {
-        this.name = "";
-        this.capacity = 0;
-        hasHDMI = false;
-        hasVGA = false;
+    public ClassRoom() {
     }
 
-    public ClassRoom(String name, int capacity) {
-        this.name = name;
-        this.capacity = capacity;
-        hasHDMI = false;
+    public ClassRoom(String name, int capacity, boolean hasHDMI, boolean hasVGA) {
+        this.name.set(name);
+        this.capacity.set(capacity);
+        this.hdmi.set(hasHDMI);
+        this.vga.set(hasVGA);
     }
 
-    public String getName() {
+
+    public StringProperty nameProperty() {
         return name;
     }
 
     public void setName(String name) {
-        this.name = name;
+        this.name.set(name);
     }
 
-    public int getCapacity() {
+
+    public IntegerProperty capacityProperty() {
         return capacity;
     }
 
     public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
-
-    public boolean isHasHDMI() {
-        return hasHDMI;
+        this.capacity.set(capacity);
     }
 
 
-    public void setHasHDMI(boolean hasHDMI) {
-        this.hasHDMI = hasHDMI;
+    public BooleanProperty hdmiProperty() {
+        return hdmi;
     }
 
-    public void setHasVGA(boolean hasVGA) {
-        this.hasVGA = hasVGA;
+    public void setHdmi(boolean hdmi) {
+        this.hdmi.set(hdmi);
     }
 
+    public BooleanProperty vgaProperty() {
+        return vga;
+    }
+    public void setVga(boolean vga) {
+        this.vga.set(vga);
+    }
+
+
+
+    @Override
     public String toString() {
-        return this.name + " " + this.capacity + " " + this.hasHDMI;
+        return "ClassRoom{" +
+                "name=" + name +
+                ", capacity=" + capacity +
+                ", hdmi=" + hdmi +
+                ", vga=" + vga +
+                '}';
     }
 }
--- src/main/java/planner/PrimaryController.java
+++ src/main/java/planner/PrimaryController.java
@@ -6,6 +6,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -16,15 +17,27 @@ import model.dao.StudentDao;
 import model.DataModel;
 
 public class PrimaryController {
+    @FXML
+    public CheckBox inputClassroomVGA;
+    @FXML
+    public CheckBox inputClassroomHDMI;
 
     private DataModel model;
-//    private ArrayList<Student> studentsTest = DataModel.getStudentAll();
+    //    private ArrayList<Student> studentsTest = DataModel.getStudentAll();
     @FXML
     private TextField inputClassroomName;
     @FXML
     private TextField inputClassroomCapacity;
     @FXML
-    private TableView<ClassRoom> tableClassroom = new TableView<>();
+    private TableView<ClassRoom> tableClassroom;
+    @FXML
+    public TableColumn<ClassRoom, String> name;
+    @FXML
+    public TableColumn<ClassRoom, Integer> capacity;
+    @FXML
+    public TableColumn<ClassRoom, Boolean> hdmi;
+    @FXML
+    public TableColumn<ClassRoom, Boolean> vga;
     @FXML
     private TextField studentIDinput;
     @FXML
@@ -44,22 +57,19 @@ public class PrimaryController {
         this.model = model;
     }
 
-    public void initialize(){
+    public void initialize() {
 
-/*        final ObservableList<Student> data = FXCollections.observableArrayList(
-                new Student(1, "A", "A"),
-                new Student(2, "B", "A"),
-                new Student(3, "C", "A"),
-                new Student(4, "D", "A"),
-                new Student(5, "F", "A"));*/
 
         studentId.setCellValueFactory(new PropertyValueFactory<Student, Integer>("studentId"));
         studentFirstName.setCellValueFactory(new PropertyValueFactory<Student, String>("studentFirstName"));
         studentLastName.setCellValueFactory(new PropertyValueFactory<Student, String>("studentLastName"));
+        name.setCellValueFactory(new PropertyValueFactory<ClassRoom, String>("name"));
+        capacity.setCellValueFactory(new PropertyValueFactory<ClassRoom, Integer>("capacity"));
+        hdmi.setCellValueFactory(new PropertyValueFactory<ClassRoom, Boolean>("hdmi"));
+        vga.setCellValueFactory(new PropertyValueFactory<ClassRoom, Boolean>("vga"));
 
-/*        studentTable.setItems(data);
-        System.out.println(data);*/
     }
+
 
     public void MethodTesting(ActionEvent actionEvent) {
         System.out.println("test");
@@ -72,17 +82,18 @@ public class PrimaryController {
     }
 
 
-
-    public void addClassroom() throws NullPointerException {
-        ClassRoom classRoom = new ClassRoom();
-        classRoom.setName(inputClassroomName.getText());
-        classRoom.setCapacity(Integer.parseInt(inputClassroomCapacity.getText()));
-        classRoom.setHasHDMI(true);
-        classRoom.setHasHDMI(false);
+    public void addClassroom() {
+        ClassRoom classRoom = new ClassRoom(inputClassroomName.getText(), Integer.parseInt(inputClassroomCapacity.getText()), false, false);
+        if (inputClassroomHDMI.isSelected())
+            classRoom.setHdmi(true);
+        if (inputClassroomVGA.isSelected())
+            classRoom.setVga(true);
         System.out.println("Classroom added");
         tableClassroom.getItems().add(classRoom);
         inputClassroomName.clear();
         inputClassroomCapacity.clear();
+        inputClassroomHDMI.setSelected(false);
+        inputClassroomVGA.setSelected(false);
     }
 
     public void addStudent() {
