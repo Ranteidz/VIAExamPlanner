311b59f9f524458f721088c54056afb258ca4868
Paul <bujor.paul2000@gmail.com>
2019-12-16 12:24:43 +0100

    removed unnecessary code

--- src/main/java/planner/LoadData.java
+++ src/main/java/planner/LoadData.java
@@ -13,22 +13,17 @@ public class LoadData extends Thread {
     }
 
     public void run() {
-        int numberOfTries = 0;
         long startTime = System.currentTimeMillis();
         boolean stop = false;
-        while (!stop && numberOfTries < 10) {
+        while (!stop) {
             while (System.currentTimeMillis() - startTime < timeDelta) ;
             try {
                 parentController.updateData();
-                numberOfTries++;
                 stop = true;
             } catch (NullPointerException e) {
                 startTime = System.currentTimeMillis();
             }
         }
-        if (numberOfTries == 10) {
-            System.out.println("Could not retrieve data from database, please check if the computer is connected to the internet, otherwise, contact system administrator.");
-        } else
             System.out.println("Data retrieved from database");
     }
 }
